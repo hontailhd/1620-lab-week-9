5f001e488eef062a281f3a661eb8028b6ebd8d56
James Kim <hontailhd@gmail.com>
2021-03-12 14:41:13 -0800

    added css for mobile use and set up a java script

---
+++ app.js
@@ -0,0 +1,16 @@
+var a = 16
+var b = 64
+const c = "hello_world"
+const d = false
+
+if  (a === b){
+	console.log("they match")
+}
+else {
+	console.log("no luck")
+}	
+
+for (let char in c) {
+	console.log(c[char])
+}
+	
